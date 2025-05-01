f26a596317b6d09972d52fa47af5f2de799bb99b
scpcom <scpcom@gmx.de>
2025-05-01 02:04:12 +0200

    package: mavlink/mavproxy/pymavlink: update depends

--- package/mavproxy/mavproxy.mk
+++ package/mavproxy/mavproxy.mk
@@ -9,6 +9,7 @@ MAVPROXY_SITE = $(call github,ArduPilot,MAVProxy,$(MAVPROXY_VERSION))
 MAVPROXY_SETUP_TYPE = setuptools
 MAVPROXY_LICENSE = GPL-2.0
 MAVPROXY_LICENSE_FILES = COPYING
+MAVPROXY_DEPENDENCIES= python3 pymavlink python-numpy python-serial
 
 $(eval $(python-package))
 
