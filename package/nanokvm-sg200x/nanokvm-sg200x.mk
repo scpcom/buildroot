34dd43388b0bb7205080fd08901f7a0a755a9492
scpcom <scpcom@gmx.de>
2025-05-26 01:27:27 +0200

    buildroot: nanokvm: remove S03usbhid from overlay

--- package/nanokvm-sg200x/nanokvm-sg200x.mk
+++ package/nanokvm-sg200x/nanokvm-sg200x.mk
@@ -157,9 +157,11 @@ define NANOKVM_SG200X_INSTALL_TARGET_CMDS
 	fi
 	mkdir -pv $(TARGET_DIR)/etc/init.d/
 	rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(TARGET_DIR)/kvmapp/system/init.d/ $(TARGET_DIR)/etc/init.d/
+	rm -f $(TARGET_DIR)/etc/init.d/S03usbhid
 	if [ -e $(NANOKVM_SG200X_EXT_OVERLAY)/etc/init.d ]; then \
 		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(TARGET_DIR)/kvmapp/system/init.d/ $(NANOKVM_SG200X_EXT_OVERLAY)/etc/init.d/ ; \
 	fi
+	rm -f $(NANOKVM_SG200X_EXT_OVERLAY)/etc/init.d/S03usbhid
 endef
 
 $(eval $(generic-package))
