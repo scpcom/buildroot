ade367bd9e1d74c42df4b1f0e7e0836d70ae782d
scpcom <scpcom@gmx.de>
2025-11-13 18:32:33 +0100

    buildroot: maix-py: run bdist_wheel

--- package/maix-py/maix-py.mk
+++ package/maix-py/maix-py.mk
@@ -27,7 +27,19 @@ endef
 MAIX_PY_POST_EXTRACT_HOOKS += MAIX_PY_POST_EXTRACT_FIXUP
 
 define MAIX_PY_BUILD_CMDS
-	# todo: maix and maixpy site-packages
+	@$(eval PYVER_COMMA=$(shell echo $(PYTHON3_VERSION) | cut -d '.' -f 1-2 | sed s/'\.'/', '/g))
+	# todo: nn/tpu
+	cd $(@D)/ ; sed -i s/'basic nn peripheral'/'basic peripheral'/g components/maix/CMakeLists.txt
+	cd $(@D)/ ; sed -i s/'voice vision_extra llm'/'voice'/g components/maix/CMakeLists.txt
+	cd $(@D)/ ; sed -i s/'voice vision_extra'/'voice'/g components/maix/CMakeLists.txt
+	cd $(@D)/ ; sed -i s/'"basic", "nn", "peripheral"'/'"basic", "peripheral"'/g components/maix/component.py
+	cd $(@D)/ ; sed -i s/'"voice", "vision_extra", "llm"'/'"voice"'/g components/maix/component.py
+	cd $(@D)/ ; sed -i s/'basic nn peripheral'/'basic peripheral'/g tools/maix_module/components/maix/CMakeLists.txt
+	cd $(@D)/ ; sed -i s/'3, 11'/'$(PYVER_COMMA)'/g setup.py
+	cd $(@D)/ ; sed -i s/'3, 11'/'$(PYVER_COMMA)'/g tools/maix_module/setup.py
+	export MAIXCDK_PATH=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION) ; \
+	cd $(@D)/ ; \
+	PATH=$(BR_PATH) $(HOST_DIR)/bin/python3 setup.py bdist_wheel maixcam
 	if [ "X$(BR2_PACKAGE_MAIX_PY_ALL_PROJECTS)" = "Xy" -a -e $(@D)/distapps.sh -a -e $(@D)/projects/build_all.sh ]; then \
 		chmod +x $(@D)/projects/build_all.sh ; \
 		cd $(@D)/projects/ ; \
@@ -46,6 +58,12 @@ define MAIX_PY_BUILD_CMDS
 endef
 
 define MAIX_PY_INSTALL_TARGET_CMDS
+	@$(eval PYVER_SHORT=$(shell echo $(PYTHON3_VERSION) | cut -d '.' -f 1-2))
+	unzip "${@D}/dist/MaixPy-*.whl" -d $(TARGET_DIR)/usr/lib/python$(PYVER_SHORT)/site-packages
+	export MAIXCDK_PATH=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION) ; \
+	if [ -e $(TARGET_DIR)/usr/lib -a ! -e ${@D}/usr/lib/libmaixcam_lib.so ]; then \
+		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $${MAIXCDK_PATH}/components/maixcam_lib/lib/libmaixcam_lib.so $(TARGET_DIR)/usr/lib/ ; \
+	fi
 	if [ -e ${@D}/dist/maixapp ]; then \
 		mkdir -pv $(TARGET_DIR)/maixapp/ ; \
 		rsync -r --verbose --links --safe-links --hard-links ${@D}/dist/maixapp/ $(TARGET_DIR)/maixapp/ ; \
