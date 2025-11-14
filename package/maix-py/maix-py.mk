49e6429ec203fb38ba776e328c83a375407b0088
scpcom <scpcom@gmx.de>
2025-11-14 02:44:11 +0100

    buildroot: maix-py: make nn optional

--- package/maix-py/maix-py.mk
+++ package/maix-py/maix-py.mk
@@ -27,17 +27,34 @@ endef
 MAIX_PY_POST_EXTRACT_HOOKS += MAIX_PY_POST_EXTRACT_FIXUP
 
 define MAIX_PY_BUILD_CMDS
+	@$(eval MAIX_PY_EXT_MAIXCDK=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION))
 	@$(eval PYVER_COMMA=$(shell echo $(PYTHON3_VERSION) | cut -d '.' -f 1-2 | sed s/'\.'/', '/g))
-	# todo: nn/tpu
-	cd $(@D)/ ; sed -i s/'basic nn peripheral'/'basic peripheral'/g components/maix/CMakeLists.txt
-	cd $(@D)/ ; sed -i s/'voice vision_extra llm'/'voice'/g components/maix/CMakeLists.txt
-	cd $(@D)/ ; sed -i s/'voice vision_extra'/'voice'/g components/maix/CMakeLists.txt
-	cd $(@D)/ ; sed -i s/'"basic", "nn", "peripheral"'/'"basic", "peripheral"'/g components/maix/component.py
-	cd $(@D)/ ; sed -i s/'"voice", "vision_extra", "llm"'/'"voice"'/g components/maix/component.py
-	cd $(@D)/ ; sed -i s/'basic nn peripheral'/'basic peripheral'/g tools/maix_module/components/maix/CMakeLists.txt
+	if [ ! -e $(MAIX_PY_EXT_MAIXCDK)/components/nn ]; then \
+		cd $(@D)/ ; \
+		sed -i s/'basic nn peripheral'/'basic peripheral'/g components/maix/CMakeLists.txt ; \
+		sed -i s/'voice vision_extra llm'/'voice'/g components/maix/CMakeLists.txt ; \
+		sed -i s/'voice vision_extra'/'voice'/g components/maix/CMakeLists.txt ; \
+		sed -i s/'"basic", "nn", "peripheral"'/'"basic", "peripheral"'/g components/maix/component.py ; \
+		sed -i s/'"voice", "vision_extra", "llm"'/'"voice"'/g components/maix/component.py ; \
+		sed -i s/'basic nn peripheral'/'basic peripheral'/g tools/maix_module/components/maix/CMakeLists.txt ; \
+		rm -rf projects/app_chat/ ; \
+		rm -rf projects/app_face_*/ ; \
+		rm -rf projects/app_hand_*/ ; \
+		rm -rf projects/app_human_pose*/ ; \
+		rm -rf projects/app_mono_depth_estimation/ ; \
+		rm -rf projects/app_ocr/ ; \
+		rm -rf projects/app_self_learn_*/ ; \
+		rm -rf projects/app_speech/ ; \
+		rm -rf projects/app_tracker_counter/ ; \
+		rm -rf projects/app_usb_hand_touch/ ; \
+		rm -rf projects/app_usb_pose_mario/ ; \
+		rm -rf projects/app_vlm/ ; \
+		rm -rf projects/app_yolo*/ ; \
+		rm -rf projects/demo_diansai_*_circle_track/ ; \
+	fi
 	cd $(@D)/ ; sed -i s/'3, 11'/'$(PYVER_COMMA)'/g setup.py
 	cd $(@D)/ ; sed -i s/'3, 11'/'$(PYVER_COMMA)'/g tools/maix_module/setup.py
-	export MAIXCDK_PATH=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION) ; \
+	export MAIXCDK_PATH=$(MAIX_PY_EXT_MAIXCDK) ; \
 	cd $(@D)/ ; \
 	PATH=$(BR_PATH) $(HOST_DIR)/bin/python3 setup.py bdist_wheel maixcam
 	if [ "X$(BR2_PACKAGE_MAIX_PY_ALL_PROJECTS)" = "Xy" -a -e $(@D)/distapps.sh -a -e $(@D)/projects/build_all.sh ]; then \
@@ -58,11 +75,11 @@ define MAIX_PY_BUILD_CMDS
 endef
 
 define MAIX_PY_INSTALL_TARGET_CMDS
+	@$(eval MAIX_PY_EXT_MAIXCDK=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION))
 	@$(eval PYVER_SHORT=$(shell echo $(PYTHON3_VERSION) | cut -d '.' -f 1-2))
 	unzip "${@D}/dist/MaixPy-*.whl" -d $(TARGET_DIR)/usr/lib/python$(PYVER_SHORT)/site-packages
-	export MAIXCDK_PATH=$(@D)/../maix-cdk-$(MAIX_CDK_VERSION) ; \
 	if [ -e $(TARGET_DIR)/usr/lib -a ! -e ${@D}/usr/lib/libmaixcam_lib.so ]; then \
-		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $${MAIXCDK_PATH}/components/maixcam_lib/lib/libmaixcam_lib.so $(TARGET_DIR)/usr/lib/ ; \
+		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(MAIX_PY_EXT_MAIXCDK)/components/maixcam_lib/lib/libmaixcam_lib.so $(TARGET_DIR)/usr/lib/ ; \
 	fi
 	if [ -e ${@D}/dist/maixapp ]; then \
 		mkdir -pv $(TARGET_DIR)/maixapp/ ; \
