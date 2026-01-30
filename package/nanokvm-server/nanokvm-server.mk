fd9a58a15cf8328728d91b0326450de8757e4479
scpcom <scpcom@gmx.de>
2026-01-30 01:28:13 +0100

    nanokvm-server: update 20251226

--- package/nanokvm-server/nanokvm-server.mk
+++ package/nanokvm-server/nanokvm-server.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NANOKVM_SERVER_VERSION = 53a800d252983944ec8bd6c22989f7c4b8f005a3
+NANOKVM_SERVER_VERSION = cc072ab15842bf73b78a27c49f7084c9c89a5d62
 NANOKVM_SERVER_SITE = $(call github,sipeed,NanoKVM,$(NANOKVM_SERVER_VERSION))
 NANOKVM_SERVER_UPDATE_URL = https://scpcom.github.io/nanokvm
 
