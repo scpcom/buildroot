0a94764ee309ffccc23b9b9d5d1e2e2965e4b48d
scpcom <scpcom@gmx.de>
2026-02-08 23:34:06 +0100

    lcdtest: use site method git

--- package/lcdtest/lcdtest.mk
+++ package/lcdtest/lcdtest.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 LCDTEST_VERSION = d6defe34015d64b70963e16ee0e1d274387ec060
-LCDTEST_SITE = $(call github,0x754C,lcdtest,$(LCDTEST_VERSION))
+LCDTEST_SITE = https://github.com/0x754C/lcdtest
+LCDTEST_SITE_METHOD = git
 
 define LCDTEST_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
