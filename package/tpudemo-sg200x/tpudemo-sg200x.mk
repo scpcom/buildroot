fc64513f59e10de6ea46f21ed3d78a0d5a034918
scpcom <scpcom@gmx.de>
2026-02-08 23:34:31 +0100

    tpudemo: use site method git

--- package/tpudemo-sg200x/tpudemo-sg200x.mk
+++ package/tpudemo-sg200x/tpudemo-sg200x.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 TPUDEMO_SG200X_VERSION = e50c97cbfc3f6018d674883a6a60bccd63bbf01d
-TPUDEMO_SG200X_SITE = $(call github,0x754C,tpudemo-sg200x,$(TPUDEMO_SG200X_VERSION))
+TPUDEMO_SG200X_SITE = https://github.com/0x754C/tpudemo-sg200x
+TPUDEMO_SG200X_SITE_METHOD = git
 
 define TPUDEMO_SG200X_INSTALL_TARGET_CMDS
 	mkdir -pv $(TARGET_DIR)/usr/bin/
